21262cea11ea0b4749a2aa7c802ec07ecab12d10
alidemirhan <alidemirhan.mobiasoft@gmail.com>
2026-02-11 17:17:51 +0300

    app setup

---
+++ database/migrations/2026_02_11_171335_create_personal_access_tokens_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable');
+            $table->text('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable()->index();
+            $table->timestamps();
+
+            $table->string('ip_address', 45);
+            $table->string('user_agent')->nullable();
+            $table->string('device_type')->nullable();
+            $table->string('device_model')->nullable();
+            $table->string('device_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+};
