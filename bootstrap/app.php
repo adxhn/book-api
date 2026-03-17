7b0baaa08fdf6037a676274e19f53fd34451d985
alidemirhan <alidemirhan.mobiasoft@gmail.com>
2026-03-17 16:14:09 +0300

    app setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(\App\Http\Middleware\AddSecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ThrottleRequestsException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => __('system.throttle'),
+                ], 429);
+            }
+        });
     })->create();
--- lang/tr/system.php
+++ lang/tr/system.php
@@ -2,4 +2,5 @@
 
 return [
     'failed' => 'Bir sorun oluştu, lütfen daha sonra tekrar deneyin.',
+    'throttle' => 'Çok fazla deneme yaptınız. Lütfen daha sonra tekrar deneyin.',
 ];
